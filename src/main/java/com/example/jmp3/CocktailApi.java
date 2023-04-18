d07b62af3dd54459d2a84043a0285664075d152b
jimin <jwjm0402@naver.com>
2023-04-18 17:39:05 +0900

    4/17 v2

--- src/main/java/com/example/jmp3/CocktailApi.java
+++ src/main/java/com/example/jmp3/CocktailApi.java
@@ -106,7 +106,7 @@ public class CocktailApi {
         }
     }
     public void addAccountUsers(String userId, String userName, String userDepartment){
-        if (userDepartment.equals("사업본부")) { //사업본부 부서 소속인 사용자만 추가
+        if (userDepartment.equals("경영지원실")) { //사업본부 부서 소속인 사용자만 추가
             JSONObject data = new JSONObject();
             ArrayList rolearr = new ArrayList();
             rolearr.add("DEVOPS");
